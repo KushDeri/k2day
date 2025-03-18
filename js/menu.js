0096a4ecaa75ebe902d0bf9f7d80565ffd062822
KushDeri <97098215+KushDeri@users.noreply.github.com>
2025-03-18 21:05:50 +0900

    .

--- js/menu.js
+++ js/menu.js
@@ -17,13 +17,6 @@ window.addEventListener("scroll", function () {
             link.style.margin = "10px 0 0";
         });
 
-        // Збільшуємо шрифт у меню
-        //menuLinks.forEach(link => {
-        //    link.style.fontSize = "18px";
-        //});
-
-        // Зменшуємо висоту меню
-        //header.style.box-shadow = "0px 2px 10px rgba(0, 0, 0, 0.2)";
     } else {
         // Повертаємо стандартні розміри
         logo.style.width = "130px";
@@ -33,10 +26,18 @@ window.addEventListener("scroll", function () {
         menuLinks.forEach(link => {
             link.style.margin = "20px 0 0";
         });
-        //menuLinks.forEach(link => {
-        //    link.style.fontSize = "16px";
-        //});
     }
+});
+
+// Открывашка меню
+//let menuOpener = document.querySelector(".icon");
+document.addEventListener("DOMContentLoaded", function () {
+    let burgerMenu = document.getElementById("burger-menu");
+    let menu = document.getElementById("menu");
+
+    burgerMenu.addEventListener("click", function () {
+        menu.classList.toggle("active");
+    });
 });
 
 document.querySelectorAll(".submenu").forEach((category) => {
@@ -51,36 +52,8 @@ document.querySelectorAll(".submenu").forEach((category) => {
         }
     });
 });
-
 document.querySelector(".dropdown").addEventListener("mouseleave", function () {
     document.querySelectorAll(".dropdown-menu, .sub-dropdown").forEach((menu) => {
         menu.style.display = "none";
     });
 });
-
-
-
-document.getElementById('burger-menu').addEventListener('click', function() {
-        document.getElementById('menu').classList.toggle('active');
-    });
-    
-    // Открывашка меню
-    function toggleMenu() {
-//    const menu = document.getElementById("menu");
-    menu.classList.toggle('responsive');
-
-    if (menu.classList.contains('responsive')) {
-        // Закрити меню
-        // Змінити іконку на бургер
-//        document.getElementById("close").style.display = "block";
-//        document.getElementById("open").style.display = "none";
-//        document.getElementById("menu").style.background = "linear-gradient(171.42deg, #C9908B 1.7%, #7F4A45 99.16%)";
-//        document.getElementById("menu").style.height = "100%";
-    } else {
-        // Відкрити меню
-        // Змінити іконку на хрестик
-//        document.getElementById("open").style.display = "block";
-//        document.getElementById("close").style.display = "none";
-//        document.getElementById("menu").style.height = "5%";
-    }
-}
