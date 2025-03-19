60f8e959cf606b30c1d4daffbbf51ba90eb17373
KushDeri <97098215+KushDeri@users.noreply.github.com>
2025-03-19 23:25:15 +0900

    .

--- js/menu_1.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("../menu.html") // Загружаем файл меню
-        .then(response => response.text()) 
-        .then(data => {
-            document.getElementById("menu-placeholder").innerHTML = data;
-        })
-        .catch(error => console.error("Ошибка загрузки меню:", error));
-});
