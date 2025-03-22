c4432cb49329445495b9fc2b96e65835c025fba4
KushDeri <97098215+KushDeri@users.noreply.github.com>
2025-03-23 04:01:46 +0900

    .

---
+++ js/katalog.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("data.json") // Загружаем JSON
+        .then(response => response.json()) // Преобразуем в объект
+        .then(data => displayProducts(data)) // Передаём в функцию
+        .catch(error => console.error("Ошибка загрузки JSON:", error));
+});
+
+function displayProducts(products) {
+    const container = document.getElementById("products-container");
+
+    products.forEach(product => {
+        const productHTML = `
+            <div class="card">
+                <a href="#"><img src="${product.image}" alt="${product.name}"></a>
+                <div class="card-info">
+                    <a href="#">${product.brand}</a>
+                    <a href="#">${product.name}</a>
+                    <a href="#">${product.category}</a>
+                    <div class="size-price">${product.price}</div>
+                    <!--<div>відгуки</div>-->
+                </div>
+              </div>
+        `;
+        container.innerHTML += productHTML;
+    });
+}
