312afe4e5195019eb0c1203ea9c8f01e37ec469f
KushDeri <97098215+KushDeri@users.noreply.github.com>
2025-03-23 04:27:22 +0900

    .

--- js/katalog.js
+++ js/katalog.js
@@ -1,26 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
     fetch("https://kushderi.github.io/k2day/data.json") // Загружаем JSON
         .then(response => response.json()) // Преобразуем в объект
-        .then(data => displayProducts(data)) // Передаём в функцию
-        .catch(error => console.error("Ошибка загрузки JSON:", error));
-});
+        .then(data => {
 
-function displayProducts(products) {
-    const container = document.getElementById("products-container");
+            function displayProducts(products) {
+                const container = document.getElementById("products-container");
 
-    products.forEach(product => {
-        const productHTML = `
-            <div class="card">
-                <a href="#"><img src="${product.image}" alt="${product.name}"></a>
-                <div class="card-info">
-                    <a href="#">${product.brand}</a>
-                    <a href="#">${product.name}</a>
-                    <a href="#">${product.category}</a>
-                    <div class="size-price">${product.price}</div>
-                    <!--<div>відгуки</div>-->
-                </div>
-              </div>
+                products.forEach(product => {
+                    const productHTML = `
+                        <div class="card">
+                            <a href="#"><img src="${product.image}" alt="${product.name}"></a>
+                            <div class="card-info">
+                                <a href="#">${product.brand}</a>
+                                <a href="#">${product.name}</a>
+                                <a href="#">${product.category}</a>
+                                <div class="size-price">${product.price}</div>
+                                <!--<div>відгуки</div>-->
+                            </div>
+                        </div>
         `;
         container.innerHTML += productHTML;
     });
 }
+
+        }) // Передаём в функцию
+        //.catch(error => console.error("Ошибка загрузки JSON:", error));
+});
+
+
