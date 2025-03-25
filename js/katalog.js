958a9ec8712d64ab1da6192402b00ecbbf3634a6
KushDeri <97098215+KushDeri@users.noreply.github.com>
2025-03-25 23:20:22 +0900

    .

--- js/katalog.js
+++ js/katalog.js
@@ -15,13 +15,16 @@ function displayProducts(products) {
                 const container = document.getElementById("products-container");
                 const brand = document.getElementById("brand-container");
 
+                // Фильтруем товары по бренду
+                const filteredBrand = products.filter(product => product.brand === selectedBrand);
+
                 products.forEach(product => {
                     const productHTML = `
                         <div class="card">
                             <a href="#"><img src="${product.image}" alt="${product.name}"></a>
                             <div class="card-info">
-                                <a href="coming_soon.html" class="brand">${product.brand}</a>
-                                <a href="product.html" class="name">${product.name}</a>
+                                <a href="brand.html" class="brand">${product.brand}</a>
+                                <a href="product.htmlbrand=${encodeURIComponent(product.brand)}" class="name">${product.name}</a>
                                 <a href="coming_soon.html" class="category">${product.category}</a>
                                 <a href="product.html" class="v">${product.v} ${product.v_2}</a>
                                 <a href="product.html" class="price">${product.price} ₴</a>
@@ -31,8 +34,10 @@ function displayProducts(products) {
                     `;
                     container.insertAdjacentHTML("beforeend", productHTML); //ALL
 
-                    if (true) {
+                    if (filteredDrand.length > 0) {
                         brand.insertAdjacentHTML("beforeend", productHTML); //BRAND
+                    } else {
+                        brandContainer.innerHTML = "<p>Товары этого бренда пока отсутствуют.</p>";
                     }
                 });
 }
