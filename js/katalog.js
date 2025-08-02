3ca6f9be1f7b3a03cca699369b4a8eeab6a5e6e1
KushDeri <97098215+KushDeri@users.noreply.github.com>
2025-08-02 16:27:02 +0200

    0

--- js/katalog.js
+++ js/katalog.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch("https://kushderi.github.io/k2day/json/katalog_all.json") // Загружаем JSON
         .then(response => response.json()) // Преобразуем в объект
         .then(data => {
-            allProducts = [...data.rand, ...data.star]; // Сохраняем JSON для последующей фильтрации
+            allProducts = Object.values(data).flat(); // Сохраняем JSON для последующей фильтрации
             displayProducts(allProducts); // ВЫЗЫВАЕМ ФУНКЦИЮ ЗДЕСЬ   
             populateFilters(allProducts); // ДЕЛАЕТ ФИЛЬТР
         }) 
